4aa9d0b253e039c022130498edfd86f563b3a836
Sean Gordon <nodrog.naes@gmail.com>
2021-07-08 21:40:39 -0400

    header updated

--- react-portfolio-app/src/components/Header.js
+++ react-portfolio-app/src/components/Header.js
@@ -1,1 +1,27 @@
 import React from 'react';
+import '../App.css';
+
+function Header() {
+  return (
+    <header>
+        <section class="page-title">
+            <img id="header-img" 
+            src="assets/images/unnamed.jpg" 
+            alt="Header Photo" />
+            <div>
+                <h2>Sean Gordon</h2>
+                <h3>Web Development Portfolio</h3>
+            </div>
+        </section>
+        <nav class="main-nav">
+            <ul>
+                <li><a href="#about-me">About Me</a></li>
+                <li><a href="#my-work">My Work</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ react-portfolio-app/src/pages/Resume.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../App.css';
+
+export default function Resume() {
+  return (
+    <div className="resume">
+        <h1>-- Coming Soon --</h1>
+    </div>
+  );
+}
