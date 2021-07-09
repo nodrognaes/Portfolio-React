d6631038e223af5f6beb3b0234eb2d6cf26a4e6e
Sean Gordon <nodrog.naes@gmail.com>
2021-07-08 22:14:30 -0400

    project updated

--- react-portfolio-app/src/components/Project.js
+++ react-portfolio-app/src/components/Project.js
@@ -1,24 +1,23 @@
 import React, { useState } from 'react';
-import NavTabs from './NavTabs';
-import Home from './pages/Home';
+import Navigation from './Navigation';
 import About from './pages/About';
-import Blog from './pages/Blog';
+import Resume from './pages/Resume';
 import Contact from './pages/Contact';
 
 export default function PortfolioContainer() {
-  const [currentPage, setCurrentPage] = useState('Home');
+  const [currentPage, setCurrentPage] = useState('About');
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
   const renderPage = () => {
-    if (currentPage === 'Home') {
-      return <Home />;
-    }
     if (currentPage === 'About') {
       return <About />;
     }
     if (currentPage === 'Work') {
       return <Work />;
     }
+    if (currentPage === 'Contact') {
+        return <Contact />;
+      }
     return <Resume />;
   };
 
