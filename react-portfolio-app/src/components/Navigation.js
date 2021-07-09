bee5f205bf66766720c8d1f0214ff75dd2c554ae
Sean Gordon <nodrog.naes@gmail.com>
2021-07-08 20:40:50 -0400

    nav updated

--- react-portfolio-app/src/components/Navigation.js
+++ react-portfolio-app/src/components/Navigation.js
@@ -1,1 +1,42 @@
 import React from 'react';
+
+function NavTabs({ currentPage, handlePageChange }) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#blog"
+          onClick={() => handlePageChange('My Work')}
+        >
+          My Work
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => handlePageChange('Contact')}
+        >
+          Contact
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#resume"
+          onClick={() => handlePageChange('Resume')}
+        >
+          Resume
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default Navigation;
